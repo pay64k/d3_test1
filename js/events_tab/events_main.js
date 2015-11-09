2b5a16ad1e670a92cd0caf9b43f61ef2dd925750
pay64k <pay64k@gmail.com>
2015-11-09 16:03:15 +0100

    events data workign
    scrolling need limits

--- js/events_tab/events_main.js
+++ js/events_tab/events_main.js
@@ -1,31 +1,96 @@
+var heightCounter = 0 ;
+var eventDistance = 100;
+var scrollSpeed = 70;
+	
+var events_data = 	[	{eventEntry: "1", eventVisible: true, _event: 	{eventType:"msgType1", eventName: "event1", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "2", eventVisible: false, _event: 	{eventType:"msgType2", eventName: "event2", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "3", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event3", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "4", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event4", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "5", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event5", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "6", eventVisible: true, _event: 	{eventType:"msgType3", eventName: "event6", node1: "NODE1", node2: "NODE2"} },
+						{eventEntry: "7", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event7", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "8", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event8", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "9", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event9", node1: "NODE1", node2: "NODE2"} }, 
+						{eventEntry: "10", eventVisible: true, _event: 	{eventType:"msgType2", eventName: "event10", node1: "NODE1", node2: "NODE2"} }, 
+					
+					];
+
 function events_init(){
+	var offsetX = 50;
+	var offsetY = 30;
+
 	var svg = d3.select("#canvas_events")
 	.style("width", "500px")
-	.append("svg")
-    	.attr("width", 500)
-    	.attr("height", 705)  
-    		.append("g")
-    			.attr("transform", "translate(" + 20 + "," + 20 + ")")
-    			.attr("id", "events_group");
+	.style("height", "700px")
+		.append("svg")
+	    	.style("display", "block") 
+	    	.style("height", "100%")
+	    	.style("width", "100%")
+	    	.on("wheel.zoom", scrollFun)
+	    		.append("g")
+	    			.attr("transform", "translate(" + offsetX + "," + offsetY + ")")
+	    			.attr("id", "events_group");
 }
 
-var events_data = [{eventType:"msg1", node1: "NODE1", node2: "NODE2"}, {eventType:"msg2", node1: "NODE1", node2: "NODE2"}, {eventType:"msg3", node1: "NODE1", node2: "NODE2"}];
-var test_data = [10,20,300];
+function scrollFun(){
+	dx = d3.event.wheelDeltaX;
+    dy = d3.event.wheelDeltaY;
 
+    //console.log(dx + " | " + dy);
+
+    var group = d3.select("#events_group");
+    var currentPostitionY = d3.transform(group.attr("transform")).translate[1];
+
+    if (dy < 0) {
+    	group
+    	.transition().duration(150)
+    	.attr("transform", "translate(" + 50 + "," + (currentPostitionY - scrollSpeed) + ")");
+    }else{
+    	group
+		.transition().duration(150)
+    	.attr("transform", "translate(" + 50 + "," + (currentPostitionY + scrollSpeed) + ")");
+    };
+}
 function update_events(){
 
-}
+heightCounter=0;
 
-var eventDistance = 100;
+	for (var i = 0; i < events_data.length; i++) {
+		if (events_data[i].eventVisible) {
+			events_data[i].x = 0;
+			events_data[i].y = eventDistance * heightCounter;
+			heightCounter++;
+		}else{
+			events_data[i].x = 0;
+			events_data[i].y = 0;
+		};
+		
+	};
 
-function testDraw(){
-	var events_groupEnter = d3.select("#events_group").selectAll("g").data(events_data).enter()
-		.append("g")
-		.attr("class", "node")
-		.attr("transform", function(d, i) { return "translate(" + 20 + "," + i * eventDistance + ")"; })
-			.append("circle")
-			.attr("r", 10);
+	var events_group = d3.select("#events_group").selectAll("g").data( events_data.filter(function(d){ return d.eventVisible ? d.eventEntry : null }), function(d) {return d.eventEntry} );
+	
+	var events_groupEnter = events_group.enter().append("g")			
+			.attr("id", function(d) { return d._event.eventName; } )
+			.attr("class", "node")
+			.attr("transform", function(d, i) { return "translate(" + (-300) + "," + d.y + ") scale(0)"; });
 			
+	events_groupEnter.append("circle")
+			.attr("r", 10);
 
-	events_groupEnter = d3.select("#events_group").selectAll("g").data(events_data).exit().remove();
+	events_groupEnter.append("text")
+			.text(function(d) { return d._event.eventName; })
+			.attr("text-anchor", "middle")
+			.attr("y",20);
+				
+
+	var events_groupUpdate = events_group.transition()
+		.duration(500)
+		.ease("bounce")
+		.attr("transform", function(d,i) { return "translate(" + d.x + "," + d.y + ") scale(1)"; });
+
+	var events_groupExit = events_group.exit().transition()
+		.duration(500)
+		.attr("transform", function(d) { return "translate(" + 300+ "," + d.y + ") scale(0)"; })
+		.remove();
 }
+
